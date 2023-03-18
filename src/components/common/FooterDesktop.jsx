b687f94904e6f6b70b374d7c90290d9404f2c1cd
Mohammad Ali Khan <xvirus.bd@gmail.com>
2023-03-18 18:07:26 +0600

    necessary setting for home components

---
+++ src/components/common/FooterDesktop.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class FooterDesktop extends Component {
+  render() {
+    return (
+      <div>FooterDesktop</div>
+    )
+  }
+}
+
+export default FooterDesktop
---
+++ src/components/common/NavMenuMobile.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class NavMenuMobile extends Component {
+  render() {
+    return (
+      <div>NavMenuMobile</div>
+    )
+  }
+}
+
+export default NavMenuMobile
