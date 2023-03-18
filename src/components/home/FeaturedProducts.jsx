067428eb024b09ad8e71363d909301f7cf86871f
Mohammad Ali Khan <xvirus.bd@gmail.com>
2023-03-18 23:52:28 +0600

    featured product part 2

--- src/components/home/FeaturedProducts.jsx
+++ src/components/home/FeaturedProducts.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Button, Card, Col, Container, Row } from 'react-bootstrap'
 
 class FeaturedProducts extends Component {
     render() {
@@ -11,8 +11,60 @@ class FeaturedProducts extends Component {
                         <p>Our exclusive colleciton.</p>
                     </div>
                     <Row>
-                        <Col>1 of 2</Col>
-                        <Col>2 of 2</Col>
+                        <Col className="p-1" key={1} xl={2} lg={2} sm={4} xs={6}>
+                            <Card className='image-box card'>
+                                <img className="center" src="https://burst.shopifycdn.com/photos/grooms-prep-kit-for-wedding.jpg?width=925&format=pjpg&exif=1&iptc=1" alt="product-image" />
+                                <Card.Body>
+                                    <p className="product-name-on-card">Watch for groom</p>
+                                    <p className="product-price-on-card">Price : $120</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="p-1" key={1} xl={2} lg={2} sm={4} xs={6}>
+                            <Card className='image-box card'>
+                                <img className="center" src="https://burst.shopifycdn.com/photos/gemstone-necklace.jpg?width=925&format=pjpg&exif=1&iptc=1" alt="product-image" />
+                                <Card.Body>
+                                    <p className="product-name-on-card">Watch for groom</p>
+                                    <p className="product-price-on-card">Price : $120</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="p-1" key={1} xl={2} lg={2} sm={4} xs={6}>
+                            <Card className='image-box card'>
+                                <img className="center" src="https://burst.shopifycdn.com/photos/gold-ring-with-white-dimonds-and-a-large-stone.jpg?width=373&format=pjpg&exif=1&iptc=1" alt="product-image" />
+                                <Card.Body>
+                                    <p className="product-name-on-card">Watch for groom</p>
+                                    <p className="product-price-on-card">Price : $120</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="p-1" key={1} xl={2} lg={2} sm={4} xs={6}>
+                            <Card className='image-box card'>
+                                <img className="center" src="https://burst.shopifycdn.com/photos/slow-fashion-coat.jpg?width=373&format=pjpg&exif=1&iptc=1" alt="product-image" />
+                                <Card.Body>
+                                    <p className="product-name-on-card">Watch for groom</p>
+                                    <p className="product-price-on-card">Price : $120</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="p-1" key={1} xl={2} lg={2} sm={4} xs={6}>
+                            <Card className='image-box card'>
+                                <img className="center" src="https://burst.shopifycdn.com/photos/black-and-white-sneakers-against-purple-and-white.jpg?width=373&format=pjpg&exif=1&iptc=1" alt="product-image" />
+                                <Card.Body>
+                                    <p className="product-name-on-card">Watch for groom</p>
+                                    <p className="product-price-on-card">Price : $120</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="p-1" key={1} xl={2} lg={2} sm={4} xs={6}>
+                            <Card className='image-box card'>
+                                <img className="center" src="https://burst.shopifycdn.com/photos/beauty-set.jpg?width=373&format=pjpg&exif=1&iptc=1" alt="product-image" />
+                                <Card.Body>
+                                    <p className="product-name-on-card">Watch for groom</p>
+                                    <p className="product-price-on-card">Price : $120</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
                     </Row>
                 </Container>
             </Fragment>
