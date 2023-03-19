41ef7a9930acfd7479ffbd8fda61f9c5ff0ab09e
Mohammad Ali Khan <xvirus.bd@gmail.com>
2023-03-19 13:49:54 +0600

    mobile menu responsive

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,25 @@
 import React, { Component, Fragment } from 'react'
 import NavMenuDesktop from '../components/common/NavMenuDesktop'
+import NavMenuMobile from '../components/common/NavMenuMobile'
 import Categories from '../components/home/Categories'
 import Collection from '../components/home/Collection'
 import FeaturedProducts from '../components/home/FeaturedProducts'
 import HomeTop from '../components/home/HomeTop'
+import HomeTopMobile from '../components/home/HomeTopMobile'
 import NewArrival from '../components/home/NewArrival'
 
 class HomePage extends Component {
   render() {
     return (
       <Fragment>
-        <NavMenuDesktop />
-        <HomeTop />
+        <div className="Desktop">
+          <NavMenuDesktop />
+          <HomeTop />
+        </div>
+        <div className="Mobile">
+          <NavMenuMobile />
+          <HomeTopMobile />
+        </div>
         <FeaturedProducts />
         <NewArrival />
         <Categories />
