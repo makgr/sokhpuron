16e7f222ec5bc87e8815fd78e98f9d071d6ae078
Mohammad Ali Khan <xvirus.bd@gmail.com>
2023-03-22 23:05:53 +0600

    contact page

--- src/route/AppRoute.js
+++ src/route/AppRoute.js
@@ -2,6 +2,7 @@ import React, { Component,Fragment } from 'react'
 import { Route, Switch } from 'react-router'
 import HomePage from '../pages/HomePage'
 import UserLoginPage from '../pages/UserLoginPage';
+import ContactPage from '../pages/ContactPage';
 
 class AppRoute extends Component {
   render() {
@@ -10,6 +11,7 @@ class AppRoute extends Component {
         <Switch>
           <Route exact path="/" component={HomePage}/>
           <Route exact path="/login" component={UserLoginPage} />
+          <Route exact path="/contact" component={ContactPage} />
         </Switch>
       </Fragment>
     )
