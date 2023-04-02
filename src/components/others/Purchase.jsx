4fcc980f8964ecd4b708950fa67a55e9cde28eb7
Mohammad Ali Khan <xvirus.bd@gmail.com>
2023-04-02 21:55:04 +0600

    placeholder loading added

--- src/components/others/Purchase.jsx
+++ src/components/others/Purchase.jsx
@@ -5,16 +5,21 @@ import axios from 'axios'
 import parse from 'html-react-parser';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import { Link } from 'react-router-dom';
 
 export class Purchase extends Component {
      constructor() {
           super();
           this.state = {
-               purchase: ""
+               purchase: "",
+               loaderDiv: "",
+               mainDiv: "d-none"
           }
      }
 
      componentDidMount() {
+
           let SiteInfoPurchase = sessionStorage.getItem("AllSiteInfo");
 
           if (SiteInfoPurchase == null) {
@@ -23,26 +28,26 @@ export class Purchase extends Component {
                     let StatusCode = response.status;
                     if (StatusCode == 200) {
                          let JsonData = (response.data)[0]['parchase_guide'];
-                         this.setState({ purchase: JsonData });
+                         this.setState({ purchase: JsonData, loaderDiv: "d-none", mainDiv: "" });
 
                          sessionStorage.setItem("SiteInfoPurchase", JsonData)
                     }
                     else {
-                         toast.error("Somthing Went Wrong.", {
+                         toast.error("Somthing Went Wrong", {
                               position: "bottom-center"
                          });
                     }
 
 
                }).catch(error => {
-                    toast.error("Somthing Went Wrong.", {
+                    toast.error("Somthing Went Wrong", {
                          position: "bottom-center"
                     });
                });
 
           }  // end If Conditon 
           else {
-               this.setState({ purchase: SiteInfoPurchase });
+               this.setState({ purchase: SiteInfoPurchase, loaderDiv: "d-none", mainDiv: "" });
           }
 
      }
@@ -51,29 +56,70 @@ export class Purchase extends Component {
           return (
                <Fragment>
                     <Container>
+
+                         <div className="breadbody">
+                              <Breadcrumb>
+                                   <Breadcrumb.Item> <Link to="/"> Home </Link> </Breadcrumb.Item>
+                                   <Breadcrumb.Item> <Link to="/purchase"> Purchase </Link> </Breadcrumb.Item>
+                              </Breadcrumb>
+                         </div>
+
+
                          <Row className="p-2">
                               <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
-                                   <h4 className="section-title-login">Purchase </h4>
-                                   <p className="section-title-contact">
-                                        {parse(this.state.purchase)}
-                                   </p>
 
+                                   <div className={this.state.loaderDiv}>
+
+                                        <div class="ph-item">
+                                             <div class="ph-col-12">
+                                                  <div class="ph-row">
+                                                       <div class="ph-col-4"></div>
+                                                       <div class="ph-col-8 empty"></div>
+                                                       <div class="ph-col-6"></div>
+                                                       <div class="ph-col-6 empty"></div>
+                                                       <div class="ph-col-12"></div>
+                                                       <div class="ph-col-12"></div>
+                                                       <div class="ph-col-12"></div>
+                                                       <div class="ph-col-12"></div>
+                                                  </div>
+                                             </div>
+                                        </div>
+
+
+                                        <div class="ph-item">
+                                             <div class="ph-col-12">
+                                                  <div class="ph-row">
+                                                       <div class="ph-col-4"></div>
+                                                       <div class="ph-col-8 empty"></div>
+                                                       <div class="ph-col-6"></div>
+                                                       <div class="ph-col-6 empty"></div>
+                                                       <div class="ph-col-12"></div>
+                                                       <div class="ph-col-12"></div>
+                                                       <div class="ph-col-12"></div>
+                                                       <div class="ph-col-12"></div>
+                                                  </div>
+                                             </div>
+                                        </div>
+
+
+
+                                   </div>
+
+
+
+
+                                   <div className={this.state.mainDiv}>
+                                        <h4 className="section-title-login">Purchase </h4>
+                                        <p className="section-title-contact">
+                                             {parse(this.state.purchase)}
+                                        </p>
+
+                                   </div>
 
                               </Col>
                          </Row>
                     </Container>
-                    <ToastContainer
-                         position="top-right"
-                         autoClose={5000}
-                         hideProgressBar={false}
-                         newestOnTop={false}
-                         closeOnClick
-                         rtl={false}
-                         pauseOnFocusLoss
-                         draggable
-                         pauseOnHover
-                         theme="colored"
-                    />
+                    <ToastContainer />
                </Fragment>
           )
      }
