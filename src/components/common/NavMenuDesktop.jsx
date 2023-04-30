4f1b7dbffd547f65fd7ecc4155bd733ec3705473
Mohammad Ali Khan <xvirus.bd@gmail.com>
2023-05-01 00:21:31 +0600

    logout

--- src/components/common/NavMenuDesktop.jsx
+++ src/components/common/NavMenuDesktop.jsx
@@ -63,8 +63,47 @@ class NavMenuDesktop extends Component {
     }
   }
 
+  logout = () => {
+    localStorage.clear();
+  }
 
   render() {
+
+    let buttons;
+    if (localStorage.getItem('token')) {
+      buttons = (
+        <div>
+          <Link to="/favourite" className="btn"><i className="fa h4 fa-heart"></i><sup><span className="badge text-white bg-danger">3</span></sup>
+          </Link>
+
+          <Link to="/notification" className="btn"><i className="fa h4 fa-bell"></i><sup><span className="badge text-white bg-danger">5</span></sup>
+          </Link>
+
+          <Link to="/profile" className="h4 btn">PROFILE</Link>
+          <Link to="/" onClick={this.logout} className="h4 btn">LOGOUT</Link>
+
+          <Link to="/cart" className="cart-btn"><i className="fa fa-shopping-cart"></i> 3 Items </Link>
+        </div>
+      )
+
+    } else {
+      buttons = (
+        <div>
+          <Link to="/favourite" className="btn"><i className="fa h4 fa-heart"></i><sup><span className="badge text-white bg-danger">3</span></sup>
+          </Link>
+
+          <Link to="/notification" className="btn"><i className="fa h4 fa-bell"></i><sup><span className="badge text-white bg-danger">5</span></sup>
+          </Link>
+
+          <Link to="/login" className="h4 btn">LOGIN</Link>
+          <Link to="/register" className="h4 btn">REGISTER</Link>
+
+          <Link to="/cart" className="cart-btn"><i className="fa fa-shopping-cart"></i> 3 Items </Link>
+        </div>
+      )
+
+    }
+
     return (
       <Fragment>
         <div className="TopSectionDown">
@@ -91,16 +130,7 @@ class NavMenuDesktop extends Component {
 
                 <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
 
-                  <Link to="/favourite" className="btn"><i className="fa h4 fa-heart"></i><sup><span className="badge text-white bg-danger">3</span></sup>
-                  </Link>
-
-                  <Link to="/notification" className="btn"><i className="fa h4 fa-bell"></i><sup><span className="badge text-white bg-danger">5</span></sup>
-                  </Link>
-                  {/* <a className="btn"><i className="fa h4 fa-mobile-alt"></i></a> */}
-                  <Link to="/login" className="h4 btn">LOGIN</Link>
-                  <Link to="/register" className="h4 btn">REGISTER</Link>
-
-                  <Link to="/cart" className="cart-btn"><i className="fa fa-shopping-cart"></i> 3  </Link>
+                  {buttons}
                 </Col>
 
               </Row>
