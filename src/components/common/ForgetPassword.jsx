c8f1517680594d1a9189d2e214b1b3d2d504f18a
Mohammad Ali Khan <xvirus.bd@gmail.com>
2023-05-05 00:37:32 +0600

    forgotpassword api added

--- src/components/common/ForgetPassword.jsx
+++ src/components/common/ForgetPassword.jsx
@@ -2,8 +2,48 @@ import React, { Component, Fragment } from 'react'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Forget from '../../assets/images/forget.jpg'
+import AppURL from '../../api/AppURL';
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 class ForgetPassword extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            message: ''
+        }
+    }
+
+    // Forget Password Form Submit Method 
+    formSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            email: this.state.email
+        }
+
+        axios.post(AppURL.UserForgetPassword, data).then(response => {
+            console.log(response);
+
+            // console.log(response);
+            this.setState({ message: response.data.message })
+
+            toast.success(this.state.message, {
+                position: "top-right"
+            });
+
+        }).catch(error => {
+            console.log(error);
+            this.setState({ message: error.response.data.message })
+            toast.error(this.state.message, {
+                position: "top-right"
+            });
+        });
+
+    }
+
     render() {
         return (
             <Fragment>
@@ -13,12 +53,12 @@ class ForgetPassword extends Component {
 
                             <Row className="text-center">
                                 <Col className="d-flex justify-content-center" md={6} lg={6} sm={12} xs={12}>
-                                    <Form className="onboardForm">
+                                    <Form className="onboardForm" onSubmit={this.formSubmit}>
                                         <h4 className="section-title-login"> Forget Password ? </h4>
 
-                                        <input className="form-control m-2" type="email" placeholder="Enter Your Email" />
+                                        <input className="form-control m-2" type="email" placeholder="Enter Your Email" onChange={(e) => { this.setState({ email: e.target.value }) }} />
 
-                                        <Button className="btn btn-block m-2 site-btn-login"> Reset Password </Button>
+                                        <Button type="submit" className="btn btn-block m-2 site-btn-login"> Reset Password </Button>
 
 
                                     </Form>
@@ -35,6 +75,7 @@ class ForgetPassword extends Component {
                         </Col>
                     </Row>
                 </Container>
+                <ToastContainer />
             </Fragment>
         )
     }
